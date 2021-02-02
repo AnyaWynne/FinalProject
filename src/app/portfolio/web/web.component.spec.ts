05cf012a8c4d2d9d363f896deefc4dddad1412fe
Anya Wynne <avkwynne@gmail.com>
2021-02-02 09:48:35 -0600

    problem with unit testing

--- src/app/portfolio/web/web.component.spec.ts
+++ src/app/portfolio/web/web.component.spec.ts
@@ -1,40 +1,79 @@
-import { ComponentFixture, TestBed, async, waitForAsync} from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 import { WebComponent } from './web.component';
 
 import { FetchService } from './fetch.service';
 
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import {
-  HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+// import { HttpClient, HttpClientModule } from '@angular/common/http';
+// import {
+//   HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 
 
 describe('WebComponent', () => {
-  let component: WebComponent;
-  let fixture: ComponentFixture<WebComponent>;
+  // let component: WebComponent;
+  // let fixture: ComponentFixture<WebComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule, HttpTestingController, HttpClientModule, HttpClient],
-      declarations: [ WebComponent ],
-      providers: [FetchService]
+      // imports: [HttpClientTestingModule, HttpTestingController, HttpClientModule, HttpClient],
+      declarations: [ WebComponent ]
+      // providers: [FetchService]
     })
     .compileComponents();
   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(WebComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  // beforeEach(() => {
+  //   fixture = TestBed.createComponent(WebComponent);
+  //   component = fixture.componentInstance;
+  //   fixture.detectChanges();
+  // });
+  it('should create the app', () => {
+    let fixture = TestBed.createComponent(WebComponent);
+    let app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
   });
-
-  it('should create web component', async()=>{
-   
-      let app = fixture.debugElement;
-      expect(app).toBeTruthy();
-    
-  });
+  
 
 });
+
+// it('should create web component', waitForAsync(()=>{
+//   let fixture = TestBed.createComponent(WebComponent);
+//   let app = fixture.debugElement.componentInstance;
+//   expect(app).toBeTruthy();
+
+// }) );
+
+
+
+// async()=>{
+   
+//   let app = fixture.debugElement;
+//   expect(app).toBeTruthy();
+
+// }
+
+
+// it(
+//   'should fetch data successfully if not called asynchronously',
+//   waitForAsync(() => {
+//     let fixture = TestBed.createComponent(UserComponent);
+//     let app = fixture.debugElement.componentInstance;
+//     let dataService = fixture.debugElement.injector.get(DataService);
+//     let spy = spyOn(dataService, 'getDetails').and.returnValue(
+//       Promise.resolve('Data')
+//     );
+//     fixture.detectChanges();
+//     fixture.whenStable().then(() => {
+//       expect(app.data).toBe('Data');
+//     });
+//   })
+// );
+
+
+
+
+
+
+
 
 // let fixture = TestBed.createComponent(WebComponent);
 // let app = fixture.debugElement;
