cde4be587a12ee03f361bb69d71989f7037ead73
Anya Wynne <avkwynne@gmail.com>
2021-02-09 10:59:43 -0600

    making more small improvements

--- src/app/portfolio/print/print.component.ts
+++ src/app/portfolio/print/print.component.ts
@@ -8,23 +8,23 @@ import { Component, OnInit } from '@angular/core';
 export class PrintComponent implements OnInit {
 
 
-  // imagePaths = [
-  //   // {path: '/assets/print/dan-mairin.jpg'},
-  //   // {path: '/assets/print/dan-mairin2.jpg'},
-  //   // {path: '/assets/print/dan-mairin3.jpg'}
+  imagePaths = [
+    // {path: '/assets/print/dan-mairin.jpg'},
+    // {path: '/assets/print/dan-mairin2.jpg'},
+    // {path: '/assets/print/dan-mairin3.jpg'}
     
-  //   // '/assets/print/dan-mairin2.jpg',
-  //   // '/assets/print/dan-mairin3.jpg',
-  //   // '/assets/print/dan-mairin4.jpg',
-  //   // '/assets/print/dan-mairin5.jpg',
-  //   // '/assets/print/dan-mairin6.jpg',
-  //   // '/assets/print/dan-mairin7.jpg',
-  //   // '/assets/print/dan-mairin8.jpg',
-  //   // '/assets/print/dan-mairin9.jpg',
-  //   // '/assets/print/dan-mairin10.jpg',
-  //   // '/assets/print/dan-mairin11.jpg',
-  //   // '/assets/print/dan-mairin12.jpg'
-  // ];
+    '/assets/print/dan-mairin2.jpg',
+    '/assets/print/dan-mairin3.jpg',
+    '/assets/print/dan-mairin4.jpg',
+    '/assets/print/dan-mairin5.jpg',
+    '/assets/print/dan-mairin6.jpg',
+    '/assets/print/dan-mairin7.jpg',
+    '/assets/print/dan-mairin8.jpg',
+    '/assets/print/dan-mairin9.jpg',
+    '/assets/print/dan-mairin10.jpg',
+    '/assets/print/dan-mairin11.jpg',
+    '/assets/print/dan-mairin12.jpg'
+  ];
 
   constructor() { }
 
